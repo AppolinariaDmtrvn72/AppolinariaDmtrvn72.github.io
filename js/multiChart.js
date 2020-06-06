3ed9c70497c0d36c0892962b1f0446dd1a3f1100
Nikita Matsnev <niki-sim@mail.ru>
2020-06-06 13:52:25 +0300

    add scrolling for multi without animation

--- js/multiChart.js
+++ js/multiChart.js
@@ -1,5 +1,6 @@
 let myChart;
-const MN1 = document.getElementById('N');
+Chart.defaults.global.animation = false;
+const MN1 = document.getElementById("N");
 const L1 = document.getElementById("L");
 
 const LambdaUP = document.getElementById("lambda1");
@@ -8,54 +9,81 @@ const LambdaDOWN = document.getElementById("lambda2");
 const MA1 = document.getElementById("a");
 const MB1 = document.getElementById("b");
 
+let count = -255,
+  multiplyer = -count;
 
+x_center = 0;
+x_range = 150;
 
 function updateChart() {
-    let wavelengthUP = Number(LambdaUP.value),
-        wavelenghtDOWN = Number(LambdaDOWN.value),
-        N = Number(MN1.value),
-        b = Number(MA1.value) * 1e-3,
-        d = Number(MB1.value) * 1e-3,
-        L = Number(L1.value) * 1e3;
+  let wavelengthUP = Number(LambdaUP.value),
+    wavelenghtDOWN = Number(LambdaDOWN.value),
+    N = Number(MN1.value),
+    b = Number(MA1.value) * 1e-3,
+    d = Number(MB1.value) * 1e-3,
+    L = Number(L1.value) * 1e3;
 
-    let labels = [];
-    let intensities = [];
+  let labels = [];
+  let intensities = [];
 
-    function intensityFunction(x) {
-        let result = 0;
-        let count = 0;
-        let e = 1e-6;
+  function intensityFunction(x) {
+    let result = 0;
+    let ccount = 0;
+    let e = 1e-6;
 
-        for (let i = wavelenghtDOWN; i <= wavelengthUP; i += 1) {
-            result += Math.pow(b, 2) * Math.pow(Math.sin((Math.PI * b * x) / (i * e * L)) / (Math.PI * b * x) / (i * e * L), 2)
-                * Math.pow(Math.sin(N * (Math.PI * d * x) / (i * e * L)) / Math.sin((Math.PI * d * x) / (i * e * L)), 2);
-            count++;
-        }
-        result = result / count;
-        intensities.push(result)
-
+    for (let i = wavelenghtDOWN; i <= wavelengthUP; i += 1) {
+      result +=
+        Math.pow(b, 2) *
+        Math.pow(
+          Math.sin((Math.PI * b * x) / (i * e * L)) /
+            (Math.PI * b * x) /
+            (i * e * L),
+          2
+        ) *
+        Math.pow(
+          Math.sin((N * (Math.PI * d * x)) / (i * e * L)) /
+            Math.sin((Math.PI * d * x) / (i * e * L)),
+          2
+        );
+      ccount++;
     }
+    result = result / ccount;
+    intensities.push(result);
+  }
 
+  x_center = parseFloat(x_center);
+  for (let x = x_center - x_range; x <= x_center + x_range; x += 0.1) {
+    x = +x.toFixed(10);
+    labels.push(+x.toFixed(2));
+    intensityFunction(x);
+  }
+  //   for (let x = (-3 / 2) * 1e2; x <= (3 / 2) * 1e2; x += 0.1) {
+  //     // x = +x.toFixed(10),
+  //     labels.push(+x.toFixed(2));
+  //     intensityFunction(x);
+  //   }
 
+  if (d > b || N === 1) {
+    myChart.data.labels = labels;
+    myChart.data.datasets[0].data = intensities;
 
-
-    for (let x = -3/2 * 1e2; x <=3/2 * 1e2 ; x += 0.1  ) {
-        // x = +x.toFixed(10),
-        labels.push(+x.toFixed(2));
-        intensityFunction(x);
-    }
-
-    if(d > b || N === 1) {
-        myChart.data.labels = labels;
-        myChart.data.datasets[0].data = intensities;
-
-        // drawMultiInterfPicture(wavelenghtDOWN, wavelengthUP, d * 1000, 3000, 1, N ,b * 1000);
-        drawMultiInterfPicture(wavelenghtDOWN, wavelengthUP, d * 100, L, 10, b * 100, N);
-    } else {
-
-        Multi.data.labels = 0;
-        Multi.data.datasets[0].data = 0;
-        drawMultiInterfPicture(0, 0, d * 100, L, 10, b * 100, N);
-    }
-    myChart.update();
+    // drawMultiInterfPicture(wavelenghtDOWN, wavelengthUP, d * 1000, 3000, 1, N ,b * 1000);
+    drawMultiInterfPicture(
+      wavelenghtDOWN,
+      wavelengthUP,
+      d * 100,
+      L,
+      10,
+      b * 100,
+      N,
+      x_center,
+      x_range
+    );
+  } else {
+    drawMultiInterfPicture(0, 0, d * 100, L, 10, b * 100, N, x_center, x_range);
+    Multi.data.labels = 0;
+    Multi.data.datasets[0].data = 0;
+    //drawMultiInterfPicture(0, 0, d * 100, L, 10, b * 100, N, x_center, x_range);
+  }
+  myChart.update();
 }
--- js/multiPicture.js
+++ js/multiPicture.js
@@ -1,15 +1,23 @@
-function drawMultiInterfPicture(wavelengthLowerBorder, wavelengthUpperBorder, d, L, wavelengthDelta, b, N) {
-
+function drawMultiInterfPicture(
+  wavelengthLowerBorder,
+  wavelengthUpperBorder,
+  d,
+  L,
+  wavelengthDelta,
+  b,
+  N,
+  x_center,
+  x_range
+) {
   function rgbMix(colors) {
     let r = 0,
-        g = 0,
-        b = 0;
+      g = 0,
+      b = 0;
 
-    colors.forEach( function (color) {
+    colors.forEach(function (color) {
       r += color.r;
       g += color.g;
       b += color.b;
-
     });
 
     r /= colors.length;
@@ -26,41 +34,58 @@ function drawMultiInterfPicture(wavelengthLowerBorder, wavelengthUpperBorder, d,
 
     let u = (Math.PI * b * x) / (length * e * L);
     let q = (Math.PI * d * x) / (length * e * L);
-    return Math.pow( b, 2) * Math.pow(Math.sin(u) / u, 2) * Math.pow(Math.sin(N * q) / Math.sin(q), 2) ;
+    return (
+      Math.pow(b, 2) *
+      Math.pow(Math.sin(u) / u, 2) *
+      Math.pow(Math.sin(N * q) / Math.sin(q), 2)
+    );
   }
   let labels = [];
 
-  for (let x = -3/2; x <= 3/2; x+=3/496) {
+  // for (let x = -3 / 2; x <= 3 / 2; x += 3 / 496) {
+  //   labels.push(x);
+  // }
+
+  let from = (x_center - x_range) / 100;
+  let to = (x_center + x_range) / 100;
+  for (let x = from; x <= to; x += N / wavelengthUpperBorder) {
     labels.push(x);
   }
 
-  const ctx = document.getElementById("interference_picture").getContext("2d"),
-      width = document.getElementById('interference_picture').offsetWidth,
-      height = document.getElementById('interference_picture').offsetHeight;
+  let ctx = document.getElementById("interference_picture").getContext("2d"),
+    width = document.getElementById("interference_picture").offsetWidth,
+    height = document.getElementById("interference_picture").offsetHeight;
 
   ctx.clearRect(0, 0, width, height);
 
-  const yStep = width / labels.length;
-  let al, max =0;
+  let yStep = width / labels.length;
+  let al,
+    max = 0;
 
-  labels.forEach(function(x, index){
-    for (let i = wavelengthLowerBorder; i <= wavelengthUpperBorder; i += wavelengthDelta) {
+  labels.forEach(function (x, index) {
+    for (
+      let i = wavelengthLowerBorder;
+      i <= wavelengthUpperBorder;
+      i += wavelengthDelta
+    ) {
       al = alpha(x, i);
-      if( al > max) {
+      if (al > max) {
         max = al;
       }
     }
   });
 
-  labels.forEach(function(x, index) {
+  labels.forEach(function (x, index) {
     const yCoord = index * yStep;
 
     let colors = [];
 
-
-    for (let i = wavelengthLowerBorder; i <= wavelengthUpperBorder; i += wavelengthDelta) {
-
-      let alp = alpha(x, i) ;
+    for (
+      let i = wavelengthLowerBorder;
+      i <= wavelengthUpperBorder;
+      i += wavelengthDelta
+    ) {
+      let alp = alpha(x, i);
 
       const currentColor = Math.nmToRGB(i);
       const currentAlpha = alp / max;
@@ -73,12 +98,11 @@ function drawMultiInterfPicture(wavelengthLowerBorder, wavelengthUpperBorder, d,
 
     let color = rgbMix(colors);
 
-    ctx.strokeStyle = 'rgb(' + color.r + ', ' + color.g + ', ' + color.b + ')';
+    ctx.strokeStyle = "rgb(" + color.r + ", " + color.g + ", " + color.b + ")";
     ctx.beginPath();
     ctx.moveTo(yCoord, 0);
     ctx.lineTo(yCoord, width);
     ctx.closePath();
     ctx.stroke();
   });
-
 }
